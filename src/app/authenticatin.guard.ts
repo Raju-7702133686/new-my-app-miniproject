9048de00a9b2894c64dfba087ad443962aefc7fc
Raju-7702133686 <Raju-rajuis819@gmail.com>
2023-07-23 19:51:19 +0530

    changes made in login

--- src/app/authenticatin.guard.ts
+++ src/app/authenticatin.guard.ts
@@ -3,14 +3,14 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authenticatinGuard: CanActivateFn = (route, state) => {
   
   return true;
-  // if(localStorage.getItem('token')){
-  //   return true;
-  // }
-  // else{
-  //   alert("You need to login to access this page");
-  //   var router: Router = new Router();
-  //   router.navigateByUrl("/login");
-  //   return false;
-  // }
+  if(localStorage.getItem('token')){
+    return true;
+  }
+  else{
+    alert("You need to login to access this page");
+    var router: Router = new Router();
+    router.navigateByUrl("/login");
+    return false;
+  }
 };
 
